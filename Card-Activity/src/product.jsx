014dfd844eeae7ebf89a44cce8b7cf9f8e48e42c
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-02 11:31:55 +0530

    cards in react

--- Card-Activity/src/product.jsx
+++ Card-Activity/src/product.jsx
@@ -1,15 +1,23 @@
 import "./Product.css";
 import Price from "./Price.jsx";
 
-function Product({title}) {
+function Product({title,idx}) {
     let oldPrices=["12,495","11,900","1,599","599"];
     let newPrices=["10,999","9,999","1,299","499"];
+    let description=[
+        ["8,000 DPI","5 programmable buttons","ergonomic design"], 
+        ["intuitive design","designed for ipad pro","palm rejection"], 
+        ["designed for ipad pro","palm rejection","tilt support"],
+         ["wireless","long battery life","compact design"]
+        ];
     return (
         <div className="product">
            <h4>{title}</h4>
-           <p>Description</p>
-           <Price />
+           <p>{description[idx][0]}</p>
+           <p>{description[idx][1]}</p>
+           <p>{description[idx][2]}</p>
+           <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
         </div>
     );
-}   
+}
 export default Product;
