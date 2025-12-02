053f4cc5431e59a8a61a620066303222ebb5ae5e
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-02 11:30:47 +0530

    cards in react

--- Card-Activity/src/ProductTab.jsx
+++ Card-Activity/src/ProductTab.jsx
@@ -1,8 +1,21 @@
+import Product from "./product.jsx";
 
 function ProductTab(){
+    let styles={
+        display:"flex",
+        flexWrap:"wrap",
+        gap:"20px",
+        justifyContent:"center",
+        marginTop:"30px"
+    };
     return(
-        <div>
-            <h2>Product Tab</h2>
-        </div>
+       <div style={styles}>
+        <Product title="logitech Mx master" idx={0}/>
+        <Product title="Apple Pencil(2nd Gen)" idx={1}/>
+        <Product title="Zebronics Zeb-transformer" idx={2}/>
+        <Product title="Petronics Toad 23" idx={3}/>
+       </div>
     );
 }
+
+export default ProductTab;
