31bc61e258dfba709e1b5c922dd0e4009362f610
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-03 23:01:29 +0530

    Added ludo buttons to understand states

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -3,17 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LikeButton from './LikeButton.jsx'
+import LudoBoard from './LudoBoard.jsx'
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>States in react</h1>
+    <LudoBoard/>
+     {/* <h1>States in react</h1>
      <p><b>Count value is: {count}</b></p>
      <LikeButton/>
      <button onClick={()=>setCount(count+1)}>Increment</button>
-     <button onClick={()=>setCount(count-1)}>Decrement</button>
+     <button onClick={()=>setCount(count-1)}>Decrement</button> */}
     </>
   )
 }
