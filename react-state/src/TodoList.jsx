def1738732bdd1fc9c3be0855a438e0803e4e324
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-06 17:42:48 +0530

    Added input in todo

--- react-state/src/TodoList.jsx
+++ react-state/src/TodoList.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList() {
-    let [todos, setTodos] = useState([]);
-    let [inputValue, setInputValue] = useState("");
+    let [todos, setTodos] = useState([{task:"Sample Task", id:uuidv4()}]);
+    let [newTodo, setNewTodo] = useState("");
     let addTodo = () => {
-        console.log("Adding todo:", inputValue);
+        setTodos([...todos, {task:newTodo,id:uuidv4()}]);
+        setNewTodo("");
     }
     let updateTodoValue = (e) => {
-        setInputValue(e.target.value);
+        setNewTodo(e.target.value);
     }
     return(
         <div>
-            <input type="text" name="todo" placeholder="Add a task" onChange={updateTodoValue}/>
+            <input type="text" name="todo" placeholder="Add a task" onChange={updateTodoValue} value={newTodo}/>
             <button onClick={addTodo} >Add</button>
            <br />
            <br />
            <hr />
            <h4>Tasks to do</h4>
            <ul>
-            {todos.map((todo)=>{
-                return <li>{todo}</li>
-            })}
+            {todos.map((todo)=>(
+                 <li key={todo.id}>{todo.task}</li>
+            ))}
            </ul>
         </div>
         )
 }
+
