2be8508409d60719536b9ee9479d80aabbd35013
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-16 20:18:23 +0530

    learning forms in react

---
+++ forms-react/src/Form.jsx
@@ -0,0 +1,8 @@
+export default function Form(){
+    return(
+        <form>
+            <input type="text" placeholder="enter full name" />
+            <button>Submit</button>
+        </form>
+    )
+}
