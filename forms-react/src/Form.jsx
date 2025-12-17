9ba52169a397248b93f9c4cf18d4bd561c9a37d8
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-17 18:56:44 +0530

    multiple forms

--- forms-react/src/Form.jsx
+++ forms-react/src/Form.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
 export default function Form(){
-    let [fullName, setFullName] = useState("");
-
-    let handleChange= (e)=>{
-        setFullName(e.target.value);
+    let [formData, setFormData]= useState({
+        fullName: "",
+        username: ""
+    });
+    let inputHandler= (e)=>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
     }
     return(
         <form>
-            <label htmlFor="username">Full Name</label> &nbsp;
-            <input type="text" id="username" placeholder="enter full name" value={fullName} onChange={handleChange}/>
+            <label htmlFor="fullName">Full Name</label> &nbsp;
+            <input type="text" id="fullName" placeholder="enter full name" value={formData.fullName} onChange={inputHandler} name="fullName"/>
+            <br/><br />
+            <label htmlFor="username">User Name</label> &nbsp;
+            <input type="text" id="username" placeholder="enter username" value={formData.username} onChange={inputHandler} name="username"/>
+            <br /> <br />
             <button>Submit</button>
         </form>
     )
