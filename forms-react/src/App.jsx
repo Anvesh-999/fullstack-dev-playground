8d7a37e14acb25d0136390224bf38223809864ff
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-18 10:02:37 +0530

    comments

--- forms-react/src/App.jsx
+++ forms-react/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Form from './Form.jsx'
+import CommentForm from './CommentForm.jsx'
+import Comment from './Comment.jsx'
 
 function App() {
   return (
     <>
-     <Form/>
+     <Comment/>
     </>
   )
 }
