4135152bae7cc5946b73f27b077acf7d8b7b2fa8
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-20 13:32:45 +0530

    dependencies in useEffect

--- forms-react/src/Counter.jsx
+++ forms-react/src/Counter.jsx
@@ -1,19 +1,26 @@
 import {useState,useEffect} from "react"
 
 export default function Counter(){
-    const [count,setCount]= useState(0);
+    const [countx,setCountx]= useState(0);
+    const [county,setCounty]= useState(0);
 
-    let incCounter=()=>{
-        setCount(incCount=>incCount+1);
+    let incCounterx=()=>{
+        setCountx(countx=>countx+1);
     }
+    let incCountery=()=>{
+        setCounty(county=>county+1);
+    }
+
     useEffect(()=>{
-        console.log("Counter updated to ", count);
-    })
+        console.log("Counter updated");
+    },[countx]);
 
     return(
         <div>
-            <h2>Count : {count}</h2>
-            <button onClick={incCounter}>+1</button>
+            <h2>Countx : {countx}</h2>
+            <button onClick={incCounterx}>+1</button>
+            <h2>County : {county}</h2>
+            <button onClick={incCountery}>+1</button>
         </div>
     )
 }
