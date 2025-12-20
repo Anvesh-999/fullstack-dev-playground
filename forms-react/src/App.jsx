69085d41747624f292856bd8584fc7ab0c38cc8c
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-20 13:21:13 +0530

    Learning useEffect

--- forms-react/src/App.jsx
+++ forms-react/src/App.jsx
@@ -5,11 +5,12 @@ import './App.css'
 import Form from './Form.jsx'
 import CommentForm from './CommentForm.jsx'
 import Comment from './Comment.jsx'
+import Counter from './Counter.jsx'
 
 function App() {
   return (
     <>
-     <Comment/>
+     <Counter/>
     </>
   )
 }
