e632513d5ff85ac8218eefabfc240e6e81883293
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-21 16:54:14 +0530

    Random joke generator

--- forms-react/src/Joke.jsx
+++ forms-react/src/Joke.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState,useEffect} from "react";
 
 export default function Joke(){
     const[joke,SetJoke] = useState({});
@@ -9,6 +9,16 @@ export default function Joke(){
         const data = await response.json();
         SetJoke({setup: data.setup, punchline: data.punchline});
     }
+
+    useEffect(()=>{
+        // async function firstJoke() {
+        //     const response = await fetch(url);
+        //     const data = await response.json();
+        //     SetJoke({setup: data.setup, punchline: data.punchline});
+        // }
+        fetchJoke();
+    },[]);
+
     return(
         <div>
             <h2>Joke Component</h2>
