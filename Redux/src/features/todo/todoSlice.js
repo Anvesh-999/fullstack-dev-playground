94bcad580bf68ec81047ffeb8d821decf623e6dd
Anvesh-999 <anvesh.a9100@gmail.com>
2026-01-10 21:48:53 +0530

    redux

---
+++ Redux/src/features/todo/todoSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: [{id: "abc", task:'demo-task', isDone: false}],
+};
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+});
